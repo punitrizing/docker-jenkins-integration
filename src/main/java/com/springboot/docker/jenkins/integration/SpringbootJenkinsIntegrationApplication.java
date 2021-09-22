66dcfcd05d7832f977fd75907af1cfa4742fd4b7
Punit Kumar Upadhyay <Punit.upadhyay@rizing.com>
2021-09-22 17:47:20 +0800

    2nd Commit: Added Port,Final Name, REST Controller Logic

--- src/main/java/com/springboot/docker/jenkins/integration/SpringbootJenkinsIntegrationApplication.java
+++ src/main/java/com/springboot/docker/jenkins/integration/SpringbootJenkinsIntegrationApplication.java
@@ -2,10 +2,18 @@ package com.springboot.docker.jenkins.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootJenkinsIntegrationApplication {
-
+	
+	@GetMapping("/message")
+	public String getMessage() {
+		return("Sample code for JAVA Springboot REST based API !");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJenkinsIntegrationApplication.class, args);
 	}
